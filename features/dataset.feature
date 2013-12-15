94f07382d71716202d731be87a0b250fc5e59012
dcaraway <dave@fogmine.com>
2013-12-14 22:48:26 -0500

    switch to python-bcrypt library to get working under windows. have security hooked in.

--- features/dataset.feature
+++ features/dataset.feature
@@ -3,10 +3,10 @@ Feature: The dataset resources
   Background:
     Given podserve is running
     Given the following users exist
-    | display_name   | email            | password |
+    | display_name   | email  | password |
     | dave | dave@fogmine.com | secret |
 
-  Scenario: All endpoints are discoverable from /
+  Scenario: All endpoints are discoverable from root
     When I get the 'root' resource
     Then I should see a link to the 'organization' resource
     And I should see a link to the 'user' resource
--- podserve/__init__.py
+++ podserve/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_oauthlib.provider import OAuth1Provider
+from flask.ext.security import Security
+import logging
 
 __author__ = 'dwcaraway'
 __credits__ = ['Dave Caraway']
 
+log = logging.getLogger(__name__)
+
 app = Flask(__name__)
 oauth = OAuth1Provider(app)
+security = Security()
 
 class DefaultConfig(object):
     MONGODB_SETTINGS = {
@@ -27,10 +32,19 @@ def init_application(config_object=DefaultConfig):
     app.config.from_object(config_object)
     oauth.init_app(app)
 
-    from podserve.model import db
+    from podserve.model import db, User, Role
     db.init_app(app)
+
+
+
+    from flask.ext.security import MongoEngineUserDatastore
+    user_datastore = MongoEngineUserDatastore(db, User, Role)
+
+    log.warn('about to call security.init_app')
+    security.init_app(app, user_datastore)
 
     #import web to initialize the routes
     import podserve.web
+
 
 init_application()
